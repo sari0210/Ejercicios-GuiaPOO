f8020eb653adf6e9bccd06f104d5535b8918ca38
Fatima <fh356165@gmail.com>
2023-02-15 16:43:29 -0600

    Ejercicio1-commit

--- Ejercicio-1.php
+++ Ejercicio-1.php
@@ -6,37 +6,49 @@ estos métodos recibirán 2 parámetros.
 
 
 
-    function Calcular($numero1, $numero2){
+ class Calculadora{
+    public $numero1;
+    public $numero2;
 
-        $suma = $numero1 + $numero2;
-        $resta = $numero1 - $numero2;
-        $multiplicacion = $numero1 * $numero2;
-        $dividir = $numero1 / $numero2;
-        
-        echo "<h1>Ejercicio 1</h1>". "<br>";
-        echo "<h1>Calculadora</h1>";
-        echo "<br>";
-        echo "El primer numero ingresado es: .$numero1";
-        echo "<br>";
-        echo "El segundo numero ingresado es: .$numero2";
-        echo "<br>";
-        echo "Suma"."<br>";
-        echo "La suma de estos numero es: .$suma.";
-        echo "<br>";
-        echo "Resta"."<br>";
-        echo "<br>";
-        echo "La resta de estos numeros es: .$resta.";
-        echo "<br>";
-        echo "Multiplicacion"."<br>";
-        echo "<br>";
-        echo "La multiplicacion de estos numeros es: .$multiplicacion.";
-        echo "<br>";
-        echo "Division"."<br>";
-        echo "<br>";
-        echo "La divicion de estos numeros es: .$dividir.";
+    
+    //suma
+   function sumar ($numero1, $numero2){
+    $resultado = $numero1 + $numero2;
 
-    }
+    echo "La suma es igual a:".$resultado."<br>";
 
-    Calcular(34, 56)
+   }
+   //resta
+   function restar ($numero1, $numero2){
+    $resultado = $numero1 - $numero2;
+
+    echo "La resta es igual a:".$resultado."<br>";
+
+   }
+    //multiplicacion
+    function multiplicacion ($numero1, $numero2){
+        $resultado = $numero1 * $numero2;
+
+        echo "La multiplicacion es igual a:".$resultado."<br>";
+    
+       }
+        //division
+   function division ($numero1, $numero2){
+    $resultado = $numero1 / $numero2;
+
+    echo "La division es igual a:".$resultado."<br>";
+
+   }
+
+}
+
+$objeto = new Calculadora();
+echo $objeto->sumar(10,20);
+echo "<br>";
+echo $objeto->restar(20,14);
+echo "<br>";
+echo $objeto->multiplicacion(4,20);
+echo "<br>";
+echo $objeto->division(400,20);
 
 ?>
